be3bed0cdb9836fd2e307ca5768a6f351c57ef90
gaotianchi <oncwnuEZoJJyB3Wk_2GibvvQeuuA@git.weixin.qq.com>
2024-07-31 15:47:55 +0800

    修复页面跳转 bug

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,13 @@ const draft = document.getElementById("draft");
 
 
 teleprompterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
     let text = draft.value.trim();
     if (text === '') {
         alert('请输入文本内容');
-        event.preventDefault();
     } else {
         sessionStorage.setItem("draft", text);
-        window.open("teleprompter.html")
+        window.location.href = "teleprompter.html"
     }
 
 })
